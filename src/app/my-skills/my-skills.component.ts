a3c7d20fcb4b21b3e988e75d005d4ea4b5168253
Robert Joseph <rojo3344@yahoo.com>
2018-11-17 18:05:23 +0100

    Modified the layout in profile and app components

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -8,27 +8,37 @@ import { Component, OnInit } from '@angular/core';
 export class MySkillsComponent implements OnInit {
 
   constructor() { }
-  showProgressbar: boolean;
-  value:any;
-  length:number;
+  value: any;
 
   /*Triggers the event that show the progress bar that indicates the skill level */
-  showLevel(value){
-    switch(value){
-        case "1":
-          this.value="Beginner";
+  showLevel(value) {
+    switch (value) {
+        case '1':
+          this.value = 'Beginner';
         break;
-        case "2":
-          this.value="Intermediate";
+        case '2':
+          this.value = 'Intermediate';
         break;
-        case "3":
-          this.value = "Advanced";
+        case '3':
+          this.value = 'Advanced';
         break;
-        default: this.value = "";
+        default: this.value = '';
+      }
+   }
+
+   showPercentage(value): any {
+      if (value === 'Advanced') {
+        return {'width': '90%'};
+      } else if (value === 'Intermediate') {
+        return {'width': '60%'};
+      } else if (value === 'Beginner') {
+        return {'width': '40%'};
+      } else {
+        return '';
       }
    }
 
   ngOnInit() {
-    this.length = "";
+
   }
 }
